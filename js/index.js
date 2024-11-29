12a8c06fa65fe08c9a3e33f171b52c3a42a69518
Dante Passalacqua <>
2024-11-29 15:38:31 -0700

    General updates

--- js/index.js
+++ js/index.js
@@ -1,44 +1,33 @@
 /* Copyright (c) 2024 Dante Passalacqua */
-
-const authorText = document.querySelector('.author-text');
 let userSettings = [];
-let cryptoSettings = [];
-let userCity;
+let userLocation;
+const authorText = document.querySelector('.author-text');
 
-if (localStorage.getItem('userCity')) {
-	userCity = localStorage.getItem('userCity');
+if (localStorage.getItem('userLocation')) {
+	userLocation = localStorage.getItem('userLocation');
 }
 
 if (localStorage.getItem('enabledSettings')) {
-	userSettings = JSON.parse(localStorage.getItem('enabledSettings'));
+	userSettings = localStorage.getItem('enabledSettings');
 } else {
 	userSettings = ['time', 'weather', 'quote'];
 }
 
-if (userSettings.includes('time')) {
-	document.body.addEventListener('load', getCurrentTime());
+function getCurrentTime() {
+	const date = new Date();
+	document.querySelector('.time').textContent = date.toLocaleTimeString(
+		'en-us',
+		{ timeStyle: 'short' }
+	);
+}
 
-	function getCurrentTime() {
-		const date = new Date();
-		document.querySelector('.time').textContent = date.toLocaleTimeString(
-			'en-us',
-			{
-				timeStyle: 'short',
-			}
-		);
-	}
+if (userSettings.includes('time')) {
+	getCurrentTime();
 	setInterval(getCurrentTime, 1000);
 }
 
-if (localStorage.getItem('cryptoSettings')) {
-	cryptoSettings = JSON.parse(localStorage.getItem('cryptoSettings'));
-} else {
-	cryptoSettings = [];
-}
-
-fetch(
-	'https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=night-sky'
-)
+fetch()
+	// 'https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=night-sky'
 	.then((res) => res.json())
 	.then((data) => {
 		document.body.style.backgroundImage = `url(${data.urls.full})`;
@@ -50,88 +39,23 @@ fetch(
 		authorText.textContent = `By: Vadim Sherbakov`;
 	});
 
-if (userSettings.includes('crypto')) {
-	if (cryptoSettings.includes('Bitcoin')) {
-		fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
-			.then((res) => {
-				if (!res.ok) {
-					throw Error('Something went wrong');
-				}
-				return res.json();
-			})
-			.then((data) => {
-				cryptoCard(data);
-			})
-			.catch((err) => console.log(err));
-	}
-
-	if (cryptoSettings.includes('Ethereum')) {
-		fetch('https://api.coingecko.com/api/v3/coins/ethereum')
-			.then((res) => {
-				if (!res.ok) {
-					throw Error('Something went wrong');
-				}
-				return res.json();
-			})
-			.then((data) => {
-				cryptoCard(data);
-			})
-			.catch((err) => console.log(err));
-	}
-
-	if (cryptoSettings.includes('Dogecoin')) {
-		fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
-			.then((res) => {
-				if (!res.ok) {
-					throw Error('Something went wrong');
-				}
-				return res.json();
-			})
-			.then((data) => {
-				cryptoCard(data);
-			})
-			.catch((err) => console.log(err));
-	}
-}
-
 if (userSettings.includes('weather')) {
-	if (JSON.parse(localStorage.getItem('locationEnabled'))) {
-		navigator.geolocation.getCurrentPosition((position) => {
-			fetch(
-				`https://api.open-meteo.com/v1/forecast?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&current_weather=true&temperature_unit=fahrenheit`
-			)
-				.then((res) => {
-					if (!res.ok) {
-						throw Error('Weather data not available');
-					}
-					return res.json();
-				})
-				.then((data) => {
-					document.querySelector('.weather').innerHTML = `
-										<p class="weather-temp">${Math.round(data.current_weather.temperature)}°F</p>`;
-				})
-				.catch((err) => console.log(err));
-		});
-	} else if (localStorage.getItem('userCity')) {
+	if (!userLocation) {
+		document.querySelector('.weather').innerHTML = `
+			<p class="weather-placeholder">Please set your location in <a href="settings.html">settings</a> to view weather information</p>
+		`;
+	} else {
+		let userCity;
 		fetch(
-			`https://geocoding-api.open-meteo.com/v1/search?name=${userCity}&count=1&language=en&format=json`
+			`https://geocoding-api.open-meteo.com/v1/search?name=${userLocation}&count=1&language=en&format=json`
 		)
-			.then((res) => {
-				if (!res.ok) {
-					throw Error('Weather data not available');
-				}
-				return res.json();
-			})
+			.then((res) => res.json())
 			.then((data) => {
+				userCity = data.results[0].name;
 				fetch(
 					`https://api.open-meteo.com/v1/forecast?latitude=${data.results[0].latitude}&longitude=${data.results[0].longitude}&current_weather=true&temperature_unit=fahrenheit`
 				)
-					.then((res) => {
-						if (!res.ok) {
-							throw Error('Weather data not available');
-						}
-						return res.json();
-					})
+					.then((res) => res.json())
 					.then((data) => {
 						document.querySelector('.weather').innerHTML = `
 										<p class="weather-temp">${Math.round(data.current_weather.temperature)}°F</p>
@@ -143,34 +67,13 @@ if (userSettings.includes('weather')) {
 	}
 }
 
-if (userSettings.includes('quote')) {
-	fetch('https://api.quotable.io/random')
-		.then((res) => {
-			if (!res.ok) {
-				throw Error("Can't get quote");
-			}
-			return res.json();
-		})
-		.then((data) => {
-			document.querySelector(
-				'.quote'
-			).innerHTML = `"${data.content}" - <b>${data.author}</b>`;
-		});
-}
-
-function cryptoCard(data) {
-	document.querySelector('.crypto').innerHTML += `
-	<div class="crypto-card">
-	<img src="${data.image.small}">
-		                <p><b>${data.localization.en}</b></p>
-		                <p><strong>Now</strong>🎯: $${data.market_data.current_price.usd.toFixed(
-							2
-						)}</p>
-		                <p><strong>24H</strong>👆: $${data.market_data.high_24h.usd.toFixed(
-							2
-						)}</p>
-		                <p><strong>24H</strong>👇: $${data.market_data.low_24h.usd.toFixed(
-							2
-						)}</p>
-										</div>`;
-}
+// if (userSettings.includes('quote')) {
+// 	fetch('https://api.quotable.io/random')
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			document.querySelector(
+// 				'.quote'
+// 			).innerHTML = `"${data.content}" - <b>${data.author}</b>`;
+// 		})
+// 		.catch((err) => console.log(err));
+// }
